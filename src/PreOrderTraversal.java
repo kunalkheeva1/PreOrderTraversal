de7e2f0c0915dd3056275cebdcd4b9b0d2344540
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 03:16:23 -0700

    created a TreeNode class to deal with the nodes

--- src/PreOrderTraversal.java
+++ src/PreOrderTraversal.java
@@ -1,3 +1,27 @@
+import java.util.List;
+
+
+ class TreeNode{
+ int val;
+ TreeNode left;
+ TreeNode right;
+ TreeNode(int val, TreeNode right, TreeNode left){
+     this.val = val;
+     this.left = left;
+     this.right =right;
+ }
+
+}
+
+
+
 public class PreOrderTraversal {
-    
+
+
+    public static List<Integer> preOrderTraverlal()
+
+
+    public static void main(String[] args) {
+
+    }
 }
