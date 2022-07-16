63305b5b09b53eca94a45961c5326e69a271767d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 03:24:15 -0700

    added the comments for the better understandings

--- src/PreOrderTraversal.java
+++ src/PreOrderTraversal.java
@@ -2,7 +2,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
-
+// get a node class which contains the left and right side of the tree as well as the value
  class TreeNode{
  int val;
  TreeNode left;
@@ -19,10 +19,12 @@ import java.util.List;
 
 public class PreOrderTraversal {
 
-
+//recursive method
      public static void preOrder(TreeNode root, ArrayList<Integer> list){
+         // if root is null then simply return
          if(root == null) return;
-
+        // otherwise add the value in the list first then move to the left and then right
+         // as per preOrderTraversal
          list.add(root.val);
          preOrder(root.left, list);
          preOrder(root.right, list);
@@ -31,6 +33,8 @@ public class PreOrderTraversal {
      }
 
     public static List<Integer> preOrderTraverlal(TreeNode root){
+
+         //call the method and return the list
         ArrayList<Integer> list = new ArrayList<>();
         preOrder(root, list);
         return list;
